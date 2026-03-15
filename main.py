701f0175f9fd07e8ab5125d35fa25c1680bee6d0
jarema962-star <jarema962@gmail.com>
2026-03-15 17:51:57 +0100

    add

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello bro!")
-print ("world")
-print ("hello")
+from datetime import datetime, date
+def get_days_from_today(date_str):
+    try:
+        given_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        today = date.today()
+        difference = today - given_date
+        return difference.days
+    except ValueError:
+        return "Invalid date format"
+    
+print(get_days_from_today("2021-10-09"))
